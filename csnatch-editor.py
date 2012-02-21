480aed20c995513d7e75befbc36fff4725ac4786
Daniel Nögel <dasdestillat@yahoo.com>
2012-02-21 15:56:14 +0100

    zooming now possible with CTRL+Mouse wheel

--- csnatch-editor.py
+++ csnatch-editor.py
@@ -10,14 +10,25 @@ import tempfile
 
 from PIL import Image
 
-MULTI = 15
-SIZE_X = 48
-SIZE_Y = 48
+class Settings(object):
+    def __init__(self):
+        self.MULTI = 15
+        self.SIZE_X = 48
+        self.SIZE_Y = 48
 
-GRID_WIDTH = 3
+        self.GRID_WIDTH = 3
 
-WIDTH = MULTI*SIZE_X+(SIZE_X+1)*GRID_WIDTH
-HEIGHT = MULTI*SIZE_Y+(SIZE_Y+1)*GRID_WIDTH
+        #~ self._WIDTH = self.MULTI*self.SIZE_X+(self.SIZE_X+1)*self.GRID_WIDTH
+        #~ self._HEIGHT = self.MULTI*self.SIZE_Y+(self.SIZE_Y+1)*self.GRID_WIDTH
+        
+    @property
+    def WIDTH(self):
+        return self.MULTI*self.SIZE_X+(self.SIZE_X+1)*self.GRID_WIDTH
+    
+    @property
+    def HEIGHT(self):
+        return self.MULTI*self.SIZE_Y+(self.SIZE_Y+1)*self.GRID_WIDTH
+
 
 TILES = {
     "Wall": "#ff0000",
@@ -86,32 +97,36 @@ def save_file(filetype):
     chooser.destroy()
     return filename
 
-class DrawThingy(gtk.DrawingArea):
-    def __init__(self):
+class DrawThingy(gtk.DrawingArea):    
+    def __init__(self, settings):
         gtk.DrawingArea.__init__(self)
 
         self.set_events(gtk.gdk.EXPOSURE_MASK
                             | gtk.gdk.LEAVE_NOTIFY_MASK
                             | gtk.gdk.BUTTON_PRESS_MASK
+                            | gtk.gdk.SCROLL_MASK
                             | gtk.gdk.POINTER_MOTION_MASK
                             | gtk.gdk.POINTER_MOTION_HINT_MASK )
         self.connect("expose_event", self.expose_event)
+        self.connect("scroll_event", self.scroll_event)
         self.connect("configure_event", self.configure_event)
         self.connect("motion_notify_event", self.motion_notify_event)
         self.connect("button_press_event", self.button_press_event)
         self.connect('realize',       self.realize_event)
+        
+        self.settings = settings
         
         self.pixmap = None
         self.current_object = "Wall"
 
 
         self.points = {}
-        for i in xrange(0, SIZE_X):
+        for i in xrange(0, self.settings.SIZE_X):
             self.points[(i, 0)] = TILES["Wall"]
-            self.points[(i, SIZE_Y-1)] = TILES["Wall"]
-        for i in xrange(0, SIZE_Y):
+            self.points[(i, self.settings.SIZE_Y-1)] = TILES["Wall"]
+        for i in xrange(0, self.settings.SIZE_Y):
             self.points[(0, i)] = TILES["Wall"]
-            self.points[(SIZE_X-1, i)] = TILES["Wall"]
+            self.points[(self.settings.SIZE_X-1, i)] = TILES["Wall"]
         for i in [0, 47]:
             self.points[(22, i)] = TILES["Floor"]
             self.points[(23, i)] = TILES["Rail"]
@@ -127,30 +142,30 @@ class DrawThingy(gtk.DrawingArea):
         #~ self.current_gc = gc
 
     def draw_grid(self, da):
-        self.current_gc.set_line_attributes(GRID_WIDTH, gtk.gdk.LINE_SOLID,
+        self.current_gc.set_line_attributes(self.settings.GRID_WIDTH, gtk.gdk.LINE_SOLID,
                                         gtk.gdk.CAP_BUTT, gtk.gdk.JOIN_MITER)
-        offset = int(GRID_WIDTH / 2.0)
+        offset = int(self.settings.GRID_WIDTH / 2.0)
         self.set_color("#000000")
-        for i, x in enumerate(w for w in range(0, WIDTH, (MULTI+GRID_WIDTH))):
-            da.window.draw_line(self.current_gc, x+offset, 0, x+offset, WIDTH)
-            da.window.draw_line(self.current_gc, 0, x++offset, HEIGHT, x+offset)
+        for i, x in enumerate(w for w in range(0, self.settings.WIDTH, (self.settings.MULTI+self.settings.GRID_WIDTH))):
+            da.window.draw_line(self.current_gc, x+offset, 0, x+offset, self.settings.WIDTH)
+            da.window.draw_line(self.current_gc, 0, x++offset, self.settings.HEIGHT, x+offset)
 
     def draw_points(self, da):
         for x, y in self.points:
             self.set_color(self.points[(x, y)])
-            da.window.draw_rectangle(self.current_gc, True, x*MULTI+(x+1)*GRID_WIDTH, y*MULTI+(y+1)*GRID_WIDTH, MULTI, MULTI)
+            da.window.draw_rectangle(self.current_gc, True, x*self.settings.MULTI+(x+1)*self.settings.GRID_WIDTH, y*self.settings.MULTI+(y+1)*self.settings.GRID_WIDTH, self.settings.MULTI, self.settings.MULTI)
 
             
     def draw_point(self, da, x, y, delete = False):
-        real_x, real_y = int(math.floor(x/(MULTI+GRID_WIDTH))), int(math.floor(y/(MULTI+GRID_WIDTH)))
-        x, y = real_x*MULTI+(real_x+1)*GRID_WIDTH, real_y*MULTI+(real_y+1)*GRID_WIDTH
+        real_x, real_y = int(math.floor(x/(self.settings.MULTI+self.settings.GRID_WIDTH))), int(math.floor(y/(self.settings.MULTI+self.settings.GRID_WIDTH)))
+        x, y = real_x*self.settings.MULTI+(real_x+1)*self.settings.GRID_WIDTH, real_y*self.settings.MULTI+(real_y+1)*self.settings.GRID_WIDTH
         
         if delete:
             tmp = self.current_object
             self.current_object = "Floor"
             self.set_color("#ffffff")
-        da.window.draw_rectangle(self.current_gc, True, x, y, MULTI, MULTI)
-        #~ da.queue_draw_area(x, y, MULTI, MULTI)
+        da.window.draw_rectangle(self.current_gc, True, x, y, self.settings.MULTI, self.settings.MULTI)
+        #~ da.queue_draw_area(x, y, self.settings.MULTI, self.settings.MULTI)
         
         self.points[(real_x, real_y)] = TILES[self.current_object]
         
@@ -164,8 +179,8 @@ class DrawThingy(gtk.DrawingArea):
 
     def configure_event(self, da, event):
         #~ x, y, width, height = da.get_allocation()
-        self.pixmap = gtk.gdk.Pixmap(da.window, WIDTH, HEIGHT)
-        self.pixmap.draw_rectangle(da.get_style().white_gc, True, 0, 0, WIDTH, HEIGHT)
+        self.pixmap = gtk.gdk.Pixmap(da.window, self.settings.WIDTH, self.settings.HEIGHT)
+        self.pixmap.draw_rectangle(da.get_style().white_gc, True, 0, 0, self.settings.WIDTH, self.settings.HEIGHT)
         
         
         return True
@@ -183,7 +198,7 @@ class DrawThingy(gtk.DrawingArea):
         return False
         
     def motion_notify_event(self, da, event):
-        real_x, real_y = int(math.floor(event.x/(MULTI+GRID_WIDTH))), int(math.floor(event.y/(MULTI+GRID_WIDTH)))
+        real_x, real_y = int(math.floor(event.x/(self.settings.MULTI+self.settings.GRID_WIDTH))), int(math.floor(event.y/(self.settings.MULTI+self.settings.GRID_WIDTH)))
         self.emit("position", real_x, real_y)
         
         if event.state & gtk.gdk.BUTTON1_MASK and self.pixmap:
@@ -198,8 +213,20 @@ class DrawThingy(gtk.DrawingArea):
             self.draw_point(da, event.x, event.y)
         elif event.button == 3 and self.pixmap:
             self.draw_point(da, event.x, event.y, delete=True)
-        
+        else:
+            print event.button
         return True
+    def scroll_event(self, da, event):
+        if event.state  & gtk.gdk.CONTROL_MASK:
+            if event.direction & gtk.gdk.SCROLL_DOWN:
+                self.settings.MULTI-=1
+            elif not event.direction | gtk.gdk.SCROLL_UP:
+                self.settings.MULTI+=1
+                
+            self.set_size_request(self.settings.WIDTH, self.settings.HEIGHT)
+            da.queue_draw_area(0, 0, self.settings.WIDTH, self.settings.HEIGHT)
+        
+        
 
 gobject.type_register(DrawThingy)
 gobject.signal_new("position", DrawThingy, gobject.SIGNAL_RUN_FIRST,
@@ -213,6 +240,8 @@ class App(object):
         self.window.set_default_size(640, 480)
         self.window.connect("destroy", lambda w: gtk.main_quit())
         
+        self.settings = Settings()
+        #~ self.settings.MULTI=3
         table = gtk.Table()
         
         self.window.add(table)
@@ -239,13 +268,13 @@ class App(object):
         
         sw = gtk.ScrolledWindow()
         sw.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
-        self.drawing_area = DrawThingy()
-        self.drawing_area.set_size_request(WIDTH, HEIGHT)
+        self.drawing_area = DrawThingy(self.settings)
+        self.drawing_area.set_size_request(self.settings.WIDTH, self.settings.HEIGHT)
         self.drawing_area.connect("position", self.position_event)
         sw.add_with_viewport(self.drawing_area)
         
         table.attach(sw, 0, 1, 1, 2, xoptions=gtk.EXPAND|gtk.FILL, yoptions=gtk.EXPAND|gtk.FILL)
-
+        
         
         bb = gtk.VButtonBox()
         for name in TILES:
@@ -253,6 +282,7 @@ class App(object):
             bb.pack_start(b, False, False)
             b.show()
             b.connect("clicked", lambda x,y:self.drawing_area.set_object(y), name)
+        bb.set_spacing(10)
         table.attach(bb, 1, 2, 1, 2, xoptions=gtk.SHRINK, yoptions=gtk.EXPAND|gtk.FILL)
         bb.set_layout(gtk.BUTTONBOX_START)
         bb.show()
@@ -346,7 +376,7 @@ class App(object):
     
     
     def save(self, filename=None):
-        img = Image.new("RGB", [SIZE_X,SIZE_Y], "white")
+        img = Image.new("RGB", [self.settings.SIZE_X,self.settings.SIZE_Y], "white")
         for coords, color in self.drawing_area.points.iteritems():
             img.putpixel(coords,  tuple(ord(c) for c in color[1:].decode('hex')))
         img.save(filename)
@@ -370,7 +400,7 @@ class App(object):
             x+=1
         
         self.drawing_area.draw_points(self.drawing_area)
-        self.drawing_area.queue_draw_area(0, 0, WIDTH, HEIGHT)
+        self.drawing_area.queue_draw_area(0, 0, self.settings.WIDTH, self.settings.HEIGHT)
         if set_filename:
             self.current_file = filename
         else:
