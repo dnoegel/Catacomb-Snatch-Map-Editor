2ddd0740eae9f110dcc4647930b252997b7fbcfa
Daniel Nögel <dasdestillat@yahoo.com>
2012-02-22 12:52:42 +0100

    modified readme

--- README.md
+++ README.md
@@ -7,6 +7,18 @@ Features
  * Load and save levels directly from the .jar-files
  * Load and save levels to .bmp files to share them with other people
 
+Multi level support
+===================
+The original version of CS does only support one level - level1.bmp.
+Several forks of the game try to implement multi level support. Right now these forks (e.g. Maescool's or Borsty's) seem to hardcode additional levels. For this reason it is not possible for the level editor, to add additional levels to the game.
+
+Anyhow it is possible to deal with those multi-level-forks:
+
+ * The level editor support reading those levels. When opening a multi-level .jar file, you will be prompted to select the level, you want to edit.
+ * When writing to a multi-level .jar file, level1.bmp (the default level) will be overwriten. 
+ 
+If you do know how to compile the source, you are also able to add you levels to the LevelList (./src/com/mojang/mojam/level/LevelList.java).
+
 Warning
 =======
  * backup your mojam.jar file, befor you edit it. This script won't do it for you
--- csnatch-editor.py
+++ csnatch-editor.py
@@ -10,6 +10,10 @@ import tempfile
 
 from PIL import Image
 
+__VERSION__ = 0.1
+__AUTHOR__  = "Daniel Nögel"
+__NAME__ = "Catacomb Snatch Map Editor"
+
 ## Stores some basic settings
 # MULTI: Zoom-level
 # SIZE_X, SIZE_Y: Width and height of the actual 1px map
@@ -22,6 +26,8 @@ class Settings(object):
         self.SIZE_Y = 48
 
         self.GRID_WIDTH = 1
+
+        self.multi_level_support = False
 
         #~ self._WIDTH = self.MULTI*self.SIZE_X+(self.SIZE_X+1)*self.GRID_WIDTH
         #~ self._HEIGHT = self.MULTI*self.SIZE_Y+(self.SIZE_Y+1)*self.GRID_WIDTH
@@ -44,26 +50,51 @@ TILES = {
     "Rail": "#969696"
 }
 
-__VERSION__ = 0.1
-__AUTHOR__  = "Daniel Nögel"
-__NAME__ = "Catacomb Snatch Map Editor"
-
 
 def rgb2hex(rgb_tuple):
     return '#%02x%02x%02x' % rgb_tuple
 
 ## Extract level1.bmp from .jar
 def get_file_from_jar(jar):
+    
+    
     tmp = tempfile.mkdtemp(prefix="csnatch.mapedit.", dir="/tmp")
     with zipfile.ZipFile(jar) as zf:
-        zf.extract("levels/level1.bmp", tmp)
-        return os.path.join(tmp, "levels", "level1.bmp")
+        
+
+        
+        
+        levels = [m for m in zf.namelist() if m.startswith("levels/") and m.endswith(".bmp")]
+        for n in levels[:]:
+            while levels.count(n) > 1:
+                levels.remove(n)
+                
+        for level in levels:
+            zf.extract(level, tmp)
+        
+        extracted_levels =  [os.path.join(tmp, "levels", l) for l in os.listdir(os.path.join(tmp, "levels"))]
+
+        if len(extracted_levels) == 1:
+            return extracted_levels[0]
+
+        l = LevelChooser(extracted_levels)
+        if l.run() == gtk.RESPONSE_OK:
+            level = l.levels[l.current_image]
+            l.destroy()
+            return level
+        else:
+            l.destroy()
+        
+        
+    
+        
 
 ## Put level1.bmp into .jar
-def save_to_jar(filename, jar):
-    print filename, jar
+def save_to_jar(filename, jar, level="level1.bmp"):
+    if not level.endswith(".bmp"): level = "{0}.bmp".format(level)
+    print filename, jar, level
     with zipfile.ZipFile(jar, "a") as zf:
-        zf.write(filename, "levels/level1.bmp")
+        zf.write(filename, "levels/{0}".format(level))
         #~ return os.path.join(tmp, "levels", "level1.bmp")
 
 ## Show file dialog
@@ -109,6 +140,68 @@ def save_file(filetype):
     if not filename.endswith(".bmp") and filetype == "bmp":
         return "{0}.bmp".format(filename)
     return filename
+
+class LevelChooser(gtk.Dialog):
+    def __init__(self, levels):
+        gtk.Dialog.__init__(self)
+        
+        self.current_image = 0
+        self.levels = levels
+
+        self.set_modal(True)
+        self.set_resizable(False)
+
+        vbox = gtk.VBox()
+        self.vbox.pack_start(vbox, False, True)
+        vbox.show()
+        
+        lbl = gtk.Label("Please select the level you want to edit\n")
+        vbox.pack_start(lbl, False, True)
+        
+        self.label = gtk.Label("")
+        vbox.pack_start(self.label, False, True)
+        self.image = gtk.Image()
+        vbox.pack_start(self.image, False, True)
+        
+            
+        
+        bb = gtk.HButtonBox()
+        btn1 = gtk.Button(stock=gtk.STOCK_GO_BACK)
+        btn2 = gtk.Button(stock=gtk.STOCK_GO_FORWARD)
+        btn1.connect("clicked", self.btn_clicked, "<")
+        btn2.connect("clicked", self.btn_clicked, ">")
+        bb.set_layout(gtk.BUTTONBOX_EDGE)
+        bb.pack_start(btn1)
+        bb.pack_start(btn2)
+        vbox.pack_start(bb, False, True)
+
+
+        self.add_button(gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL)
+        self.add_button(gtk.STOCK_OK, gtk.RESPONSE_OK)
+        
+        
+        self.set_image(self.levels[0])
+        self.show_all()
+    
+    def set_image(self, img):
+        pb = gtk.gdk.pixbuf_new_from_file_at_size(img, 240, 240)
+        self.image.set_from_pixbuf(pb)
+        self.label.set_text(os.path.basename(img))
+        self.set_title("Level {0}/{1}".format(self.current_image+1, len(self.levels)))
+
+    def btn_clicked(self, btn, user):
+        if user == "<":
+            if self.current_image == 0: 
+                self.current_image = len(self.levels)-1
+            else:
+                self.current_image -= 1
+        elif user == ">":
+            if self.current_image >= len(self.levels)-1:
+                self.current_image = 0
+            else:
+                self.current_image += 1
+        self.set_image(self.levels[self.current_image])
+
 
 ## My drawing area
 class DrawThingy(gtk.DrawingArea):    
@@ -180,6 +273,9 @@ class DrawThingy(gtk.DrawingArea):
     ## Draw a single point
     def draw_point(self, da, x, y, delete = False):
         real_x, real_y = int(math.floor(x/(self.settings.MULTI+self.settings.GRID_WIDTH))), int(math.floor(y/(self.settings.MULTI+self.settings.GRID_WIDTH)))
+        
+        if real_x >= self.settings.SIZE_X or real_y >= self.settings.SIZE_Y: return
+        
         x, y = real_x*self.settings.MULTI+(real_x+1)*self.settings.GRID_WIDTH, real_y*self.settings.MULTI+(real_y+1)*self.settings.GRID_WIDTH
         
         if delete:
@@ -224,7 +320,7 @@ class DrawThingy(gtk.DrawingArea):
         return False
         
     def motion_notify_event(self, da, event):
-        if event.x >= self.settings.WIDTH: return
+        #~ if event.x >= self.settings.WIDTH: return
         real_x, real_y = int(math.floor(event.x/(self.settings.MULTI+self.settings.GRID_WIDTH))), int(math.floor(event.y/(self.settings.MULTI+self.settings.GRID_WIDTH)))
         self.emit("position", real_x, real_y)
         
@@ -237,7 +333,7 @@ class DrawThingy(gtk.DrawingArea):
         
     def button_press_event(self, da, event):
         
-        if event.x >= self.settings.WIDTH: return
+        #~ if event.x >= self.settings.WIDTH: return
         if event.button == 1 and self.pixmap:
             self.draw_point(da, event.x, event.y)
         elif event.button == 3 and self.pixmap:
@@ -277,6 +373,7 @@ class App(object):
         
         self.window.add(table)
         
+        
         #
         # Menu
         #
@@ -301,6 +398,37 @@ class App(object):
         menu_bar.show()
         
         #
+        # Check
+        #
+        #~ hbox = gtk.HBox()
+        #~ check = gtk.CheckButton("Enable multi-level support")
+        #~ check.set_tooltip_text("Not available for the original Catacomb Snatch version made by Mojang")
+        #~ hbox.pack_start(check, False, True)
+        #~ check.show()
+        #~ 
+        #~ self.level_name = gtk.Entry()
+        #~ self.level_name.set_text("asd")
+        #~ hbox.pack_end(self.level_name, False, True)
+        #~ lbl_level_name = gtk.Label("Level name: ")
+        #~ hbox.pack_end(lbl_level_name, False, True)
+#~ 
+        #~ def toggle_event(widget):
+            #~ active = widget.get_active()
+            #~ self.settings.multi_level_support = active
+            #~ if active:
+                #~ lbl_level_name.show()
+                #~ self.level_name.show()
+            #~ else:
+                #~ lbl_level_name.hide()
+                #~ self.level_name.hide()
+        #~ check.connect("toggled", toggle_event)
+        #~ 
+        #~ hbox.show()
+        #~ table.attach(hbox, 0, 2, 1, 2, xoptions=gtk.FILL|gtk.EXPAND, yoptions=gtk.SHRINK)
+        
+        
+        
+        #
         # SW
         #
         sw = gtk.ScrolledWindow()
@@ -310,7 +438,7 @@ class App(object):
         self.drawing_area.connect("position", self.position_event)
         sw.add_with_viewport(self.drawing_area)
         
-        table.attach(sw, 0, 1, 1, 2, xoptions=gtk.EXPAND|gtk.FILL, yoptions=gtk.EXPAND|gtk.FILL)
+        table.attach(sw, 0, 1, 2, 3, xoptions=gtk.EXPAND|gtk.FILL, yoptions=gtk.EXPAND|gtk.FILL)
         
         #
         # Tiles
@@ -326,13 +454,13 @@ class App(object):
             b.show()
             b.connect("clicked", lambda x,y:self.drawing_area.set_object(y), name)
         bb.set_spacing(10)
-        table.attach(bb, 1, 2, 1, 2, xoptions=gtk.SHRINK, yoptions=gtk.EXPAND|gtk.FILL)
+        table.attach(bb, 1, 2, 2, 3, xoptions=gtk.SHRINK, yoptions=gtk.EXPAND|gtk.FILL)
         bb.set_layout(gtk.BUTTONBOX_START)
         bb.show()
 
         
         self.statusbar = gtk.Statusbar()
-        table.attach(self.statusbar, 0, 2, 2, 3, xoptions=gtk.EXPAND|gtk.FILL, yoptions=gtk.SHRINK)
+        table.attach(self.statusbar, 0, 2, 3, 4, xoptions=gtk.EXPAND|gtk.FILL, yoptions=gtk.SHRINK)
         self.statusbar.show()
         
         self.window.show()
@@ -427,7 +555,10 @@ class App(object):
                 #~ print tmp
                 self.save(tmp, set_filename=False)
                 
-                save_to_jar(tmp, filename)
+                if self.settings.multi_level_support:
+                    save_to_jar(tmp, filename, self.level_name.get_text())
+                else:
+                    save_to_jar(tmp, filename)
         elif user == "Quit":
             self.window.destroy()
         elif user == "Clear map":
